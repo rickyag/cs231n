bf82d03e93041dd51608daf19a93f80ac01cf63d
Ricky Gunadi <rickyag@Rickys-MacBook-Pro.local>
2017-10-03 17:09:15 +0700

    Finished softmax

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -34,11 +34,38 @@ def softmax_loss_naive(W, X, y, reg):
   num_classes = W.shape[1]
   for i in xrange(num_train):
     scores = X[i].dot(W)
-    correct_class_score = scores[y[i]]
-    loss += -np.log(np.exp(correct_class_score)/np.sum(np.exp(scores)))
+    scores -= np.max(scores)
+    #correct_class_score = scores[y[i]]
+    #num = np.exp(correct_class_score)
+    scores_exp = np.exp(scores)
+    denum = np.sum(scores_exp)
+    invden = 1.0/denum
+    f = scores_exp * invden
+    loss += -np.log(f[y[i]])
+    
+    f[y[i]] -= 1
+    
+    for j in xrange(num_classes):
+      dW[:,j] += X[i,:] * f[j]
+    #loss += -np.log(np.exp(correct_class_score)/np.sum(np.exp(scores)))
+    #dloss = -1/f
+    #dnum = invden * dloss
+    #dinvden = num * dloss
+    #ddenum = (-1.0 / (denum**2)) * dinvden
+    #dscores_exp = scores_exp * ddenum
+    #dscores = np.exp(scores) * dscores_exp
+    #temp = X[i].reshape(X[i].shape[0], 1)
+    #dscores = dscores.reshape(dscores.shape[0], 1)
+    #dW += temp.dot(dscores.T)
+    
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
+
+  loss /= num_train
+  loss += 0.5 * reg * np.sum(W*W)
+  dW /= num_train
+  dW += reg * W
 
   return loss, dW
 
@@ -52,6 +79,7 @@ def softmax_loss_vectorized(W, X, y, reg):
   # Initialize the loss and gradient to zero.
   loss = 0.0
   dW = np.zeros_like(W)
+  num_train = X.shape[0]
 
   #############################################################################
   # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
@@ -59,7 +87,16 @@ def softmax_loss_vectorized(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  scores = X.dot(W)
+  scores -= np.max(scores,axis=1,keepdims=True)
+  f = np.exp(scores) / np.sum(np.exp(scores),axis=1,keepdims=True)
+  correct_class_probabilities = f[range(num_train),y]
+
+  loss = np.sum(-np.log(correct_class_probabilities))/num_train
+  loss += 0.5 * reg * np.sum(W*W)
+
+  f[range(num_train),y] -=1
+  dW = X.T.dot(f)/num_train
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
